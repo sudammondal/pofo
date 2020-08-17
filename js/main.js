bf5822e0265e96c42f4a655422353a97619637e2
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-08-17 22:26:31 +0600

    counting add

--- js/main.js
+++ js/main.js
@@ -6,5 +6,12 @@ $(document).ready(function () {
             $(".header").removeClass("fixedMenu");
         }
     });
-  
-})
+
+	jQuery(document).ready(function($) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+
+});
