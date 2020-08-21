86360dfc01696f5a32dbaf57048d86e99a4a02f9
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-08-21 13:22:25 +0600

    add up_iconFixd

--- js/main.js
+++ js/main.js
@@ -13,5 +13,22 @@ $(document).ready(function () {
             time: 1000
         });
     });
+    $('.up_icon a[href^="#"').on('click', function (event) {
+		var target = $(this.getAttribute('href'));
+		if (target.length) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+    });
+    $(window).scroll(function () {
+		if ($(window).scrollTop() > 70) {
+			$(".up_icon").addClass("up_iconFixd");
+		}
+		else {
+			$(".up_icon").removeClass("up_iconFixd");
+		}
+	});
 
 });
