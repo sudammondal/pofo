2f0f81750c703d2076ac747fa43d50d519e87d84
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-09-19 15:56:17 +0600

    add font weight

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //header js//
     $(window).scroll(function () {
         if ($(window).scrollTop() > 0) {
             $(".header").addClass("fixedMenu");
@@ -13,22 +14,45 @@ $(document).ready(function () {
             $(".header2").removeClass("fixedMenu");
         }
     });
-
+    
+  // counting ja //
 	jQuery(document).ready(function($) {
         $('.counter').counterUp({
             delay: 10,
             time: 1000
         });
     });
-    $('.up_icon a[href^="#"').on('click', function (event) {
-		var target = $(this.getAttribute('href'));
-		if (target.length) {
-			event.preventDefault();
-			$('html, body').stop().animate({
-				scrollTop: target.offset().top
-			}, 2000);
-		}
+
+  // down icon js //
+    $('.dawn_icon a[href^="#"').on('click', function (event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body, section').stop().animate({
+          scrollTop: target.offset().top
+        }, 2000);
+      }
     });
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 50) {
+        $(".dawn_icon").addClass("dawn_iconFixd");
+      }
+      else {
+        $(".dawn_icon").removeClass("dawn_iconFixd");
+      }
+    });
+
+  // up icon js //
+    $('.up_icon a[href^="#"').on('click', function (event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 2000);
+      }
+    });
+
     $(window).scroll(function () {
 		if ($(window).scrollTop() > 70) {
 			$(".up_icon").addClass("up_iconFixd");
@@ -37,6 +61,8 @@ $(document).ready(function () {
 			$(".up_icon").removeClass("up_iconFixd");
 		}
     });
+
+    // slider js// 
     $('.main_slieda_ttstmonials').slick({
         autoplay: true,
         slidesToScroll: 1,
