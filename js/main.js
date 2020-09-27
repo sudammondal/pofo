6e109e14488fab92632c24a55caf80a89b5a156a
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-09-27 16:54:59 +0600

    add back ground color

--- js/main.js
+++ js/main.js
@@ -102,5 +102,45 @@ $(document).ready(function () {
           }
         ]
     });
+    
+  $('.sssyour-class').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      }
+    ]
+  });
 
 });
